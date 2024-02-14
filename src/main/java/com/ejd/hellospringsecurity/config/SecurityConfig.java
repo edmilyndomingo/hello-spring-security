01424bfa0a2cc5f1f5eb6283681c96df6a79eacb
Edmilyn Domingo <edmilyn.domingo@gmail.com>
2024-02-14 20:30:54 +0800

    Store user credentials in a db and use JdbcUserDetailsManager

--- src/main/java/com/ejd/hellospringsecurity/config/SecurityConfig.java
+++ src/main/java/com/ejd/hellospringsecurity/config/SecurityConfig.java
@@ -4,12 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
@@ -25,10 +26,8 @@ public class SecurityConfig {
   }
 
   @Bean
-  public InMemoryUserDetailsManager userDetailsManager () {
-    UserDetails admin = User.withUsername("admin").password("12345").authorities("admin").build();
-    UserDetails user = User.withUsername("user").password("12345").authorities("read").build();
-    return new InMemoryUserDetailsManager(admin, user);
+  public UserDetailsService userDetailsService(DataSource dataSource) {
+    return new JdbcUserDetailsManager(dataSource);
   }
 
   @Bean
