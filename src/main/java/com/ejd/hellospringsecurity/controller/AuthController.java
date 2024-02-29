dbb318b8266731b01895a4a3d0f6abe33d6cd7ee
Edmilyn Domingo <edmilyn.domingo@gmail.com>
2024-02-29 21:44:15 +0800

    Use BCryptPasswordEncoder

--- src/main/java/com/ejd/hellospringsecurity/controller/AuthController.java
+++ src/main/java/com/ejd/hellospringsecurity/controller/AuthController.java
@@ -5,6 +5,7 @@ import com.ejd.hellospringsecurity.repository.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +17,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   private final UserService userService;
+  private final PasswordEncoder passwordEncoder;
 
   @PostMapping("/register")
   public ResponseEntity<String> register(@RequestBody User user) {
     try {
+      user.setPassword(passwordEncoder.encode(user.getPassword()));
       User createdUser = userService.createUser(user);
       return ResponseEntity.status(HttpStatus.CREATED)
           .body("User has been successfully created" + createdUser.getId());
